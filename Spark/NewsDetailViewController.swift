8fe9717abe8e1de59646bc50375ce4d396b7fc2f
Nathan Wallace <techietotoro@gmail.com>
2016-04-11 17:47:15 -0700

    adds detail view class

---
+++ Spark/NewsDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NewsDetailViewController.swift
+//  Spark
+//
+//  Created by Nathan Wallace on 4/11/16.
+//  Copyright © 2016 Spark SC. All rights reserved.
+//
+
+import UIKit
+
+class NewsDetailViewController: UIViewController {
+
+}
