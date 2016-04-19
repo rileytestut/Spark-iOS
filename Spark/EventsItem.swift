639c1ac2ebefd40af1dcfa3837d9926b751bce61
Suveena Thanawala <Suveena@wireless-plus-072-012.usc.edu>
2016-04-18 18:30:22 -0700

    Added Events Detail View Controller

---
+++ Spark/EventsItem.swift
@@ -0,0 +1,19 @@
+//
+//  EventsItem.swift
+//  Spark
+//
+//  Created by Suveena Thanawala on 4/18/16.
+//  Copyright © 2016 Spark SC. All rights reserved.
+//
+
+import Foundation
+
+struct EventsItem {
+    
+    let title: String
+    let date: NSDate
+    let time: String
+    let location: String
+    let description: String
+    
+}
